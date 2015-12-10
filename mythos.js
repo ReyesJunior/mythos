414a2ef8ced113d4105b5652bb26332220d1dae5
Jorge Reyes Jr <reyes.junior.27@gmail.com>
2015-12-10 14:40:43 -0600

    added js code to hide panels after a level/character is selected and to alert user to make another selection from next panel (up to level selection)

--- mythos.js
+++ mythos.js
@@ -2,38 +2,39 @@
 
 $(document).ready(function () {
 
-// promp User when page loads
-
-prompt ()
-
 // Global JS
 
 // Toggle Mobile menu when clicking mobile-menu-button
-
   $('.mobile-menu-button').click( function() {
     $('.mobile-menu').animate({height: 'toggle'}, 250, function () {
     });
   });
 
 // Toggle Mobile menu when clicking gi-mobile-menu-item
-
   $('.mobile-menu-item').click( function() {
     $('.mobile-menu').animate({height: 'toggle'}, 250, function () {
     });
   });
 
 // Hide gi-mobile-menu when any anchor is clicked 
-
 $('a').click(function() {
   $('.mobile-menu').hide();
 });
 
 // When play button is pressed 
-
 $('.button-text').click(function () {
   $('.play-button').hide();
+  // alert User to select character
+  alert ("Welcome to Mythos. please select a character from the following CHARACTER SELECTION PANEL.")
   $('.character-selection-panel').show();
+});
 
+// When character is selected 
+$('.character-card').click(function () {
+  $('.character-selection-panel').hide();
+  // alert User to select level
+  alert ("Please select a level from the following LEVEL SELECTION PANEL.")
+  $('.level-selection-panel').show();
 });
 
 // On window resize 
